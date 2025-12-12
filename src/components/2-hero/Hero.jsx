bca293aa42759b0f5056c9f8febdff1110f9691f
Ismail Alaa <ismaeelalaa63@gmail.com>
2025-12-12 03:32:58 +0200

    Initial commit

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,93 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { FiDownload } from "react-icons/fi"; // أيقونة download
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section">
+        {/* الصورة والأفاتار */}
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./me.jpg"
+            className="avatar"
+            alt="My Avatar"
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        {/* العنوان */}
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          I’m Ismail Alaa – Front-End Developer
+        </motion.h1>
+
+        {/* الوصف */}
+        <p className="sub-title">
+          I create modern, responsive, and high-performance websites using React
+          and Next.js. Passionate about turning ideas into interactive web
+          experiences, I specialize in building clean and scalable front-end
+          solutions that delight users. Welcome to my portfolio!
+        </p>
+
+        {/* زرار Download CV مع أيقونة */}
+        <a href="#" download>
+          <button className="hero-download-cv-btn">
+            Download CV <FiDownload style={{ marginLeft: "0.5rem" }} />
+          </button>
+        </a>
+
+        {/* أيقونات التواصل الاجتماعي */}
+        <div className="all-icons flex">
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <div className="icon icon-twitter"></div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <div className="icon icon-instagram"></div>
+          </a>
+          <a
+            href="https://github.com/ismail-alaa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="icon icon-github"></div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ismail-eldeghedy-9430542a6/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="icon icon-linkedin"></div>
+          </a>
+        </div>
+      </div>
+
+      {/* القسم الخاص بالـ Lottie Animation */}
+      <div className="right-section animation">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,119 @@
+export const myProjects = [
+  {
+    projectTitle: "HTML & CSS",
+    projectSub: "FITZONE – Gym Website. A fully responsive gym landing page built with HTML and CSS, showcasing services and membership plans in a clean layout.",
+     projectLinks: {
+      github: "https://github.com/ismail-alaa/FITZONE",
+      live: "https://fitzone-vert.vercel.app/"
+    },
+    category: ["css"],
+    imgPath: "./images/1.jpg",
+  },
+
+  {
+    projectTitle: "HTML & CSS",
+     projectSub: "Mealify – Restaurant Website. A fully responsive restaurant landing page built with HTML and CSS, showcasing menu items and key features in a clean layout.",
+    
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/Mealify",
+      live: "https://mealify-taupe.vercel.app/"
+    },
+     category: [ "css"],
+    imgPath: "./images/2.jpg",
+  },
+
+  { 
+    projectTitle: "HTML & CSS",
+      projectSub: "Bezel – Digital Services Website. A fully responsive digital services landing page built with HTML and CSS and Bootstrap, showcasing services, pricing plans, and contact info in a clean layout.", 
+      
+       projectLinks: {
+      github: "https://github.com/ismail-alaa/Bezel",
+      live: "https://bezel-three.vercel.app/"
+    },
+      category: ["css"],
+       imgPath: "./images/3.jpg",
+       },
+  
+
+
+  //---------------------
+  //  js
+  // ----------------// 
+
+  
+  {
+    projectTitle: "JavaScript",
+     projectSub: "WeatherApp – Weather Website. A fully responsive weather landing page built with JavaScript, showcasing current weather information and forecasts in a clean layout.",
+    
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/weather-app",
+      live: "https://weather-app-sepia-delta-11.vercel.app/"
+    },
+     category: ["js"],
+    imgPath: "./images/4.jpg",
+  },
+
+  { 
+    projectTitle: "JavaScript", 
+     projectSub: "BookMarker – CRUD Website. A fully responsive CRUD website built with JavaScript, showcasing the ability to add, edit, and delete bookmarks in a clean layout.",
+     
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/Bookmarker",
+      live: "https://bookmarker-jet.vercel.app/"
+    },
+     category: ["js"],
+       imgPath: "./images/5.jpg"
+       },
+
+
+  //-------------------------------------- 
+  //           react
+  // --------------------------------
+
+  {
+    projectTitle: "React",
+     projectSub: "Loopie – Social Media Website. A fully responsive social media app built with React, showcasing posts, user interactions, and a clean user interface.",
+    
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/social-media2",
+      live: "https://social-media2-alpha.vercel.app/"
+    },
+     category: ["react"],
+    imgPath: "./images/6.jpg",
+  },
+  {
+    projectTitle: "React",
+     projectSub: "Note App – Notes Website. A fully responsive note-taking app built with React, showcasing the ability to add, edit, and delete notes in a clean interface.",
+    
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/note-app",
+      live: "https://note-app-lime-six.vercel.app/"
+    },
+     category: ["react"],
+    imgPath: "./images/7.jpg",
+  },
+  {
+    projectTitle: "React",
+     projectSub: "Corporate – Company Website. A fully responsive corporate landing page built with React, featuring a clean layout to showcase company information and services.",
+    
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/corporate",
+      live: "https://corporate-nine-phi.vercel.app/"
+    },
+     category: ["react"],
+    imgPath: "./images/8.jpg",
+  },
+
+  // Next // 
+  {
+    projectTitle: "Next.Js",
+     projectSub: "Trend Cart – E-commerce Website. A fully responsive e-commerce website built with Next.js, showcasing a wide range of products with a clean and user-friendly shopping interface.",
+  
+      projectLinks: {
+      github: "https://github.com/ismail-alaa/E-commerce-4",
+      live: "https://e-commerce-4-phi.vercel.app/"
+    },
+     category: ["next"],
+    imgPath: "./images/9.jpg",
+  },
+];
